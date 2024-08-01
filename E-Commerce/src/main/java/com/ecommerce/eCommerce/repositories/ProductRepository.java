33debbaca47d0972590fffc5c7effd6d8c791eeb
digambarExcite <digambar@excitesys.com>
2024-08-02 00:00:44 +0530

    Revert "Initial commit"
    This reverts commit d35cdeddfceb904e6ea48838cd20cf155fbc14ea.

--- E-Commerce/src/main/java/com/ecommerce/eCommerce/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.eCommerce.repositories;
-
-import com.ecommerce.eCommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-}
